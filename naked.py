0e2d3ae5eb8fdd4986feb61a6a2dd8e769e2330e
RenateLocmele <renate.locmele@va.lv>
2023-10-30 20:33:48 +0000

    Added external config reading.

--- naked.py
+++ naked.py
@@ -1,4 +1,3 @@
-
 import requests
 import json
 import datetime
@@ -6,14 +5,23 @@ import time
 import yaml
 
 from datetime import datetime
+from configparser import ConfigParser
+
 print('Asteroid processing service')
 
 # Initiating and reading config values
 print('Loading configuration from file')
 
-# Variable for NASA API key and URL to NASA API website
-nasa_api_key = "aKnaMof2TLlWeZ58ghYSXf52VPDPN1UBl8XCeE8i"
-nasa_api_url = "https://api.nasa.gov/neo/"
+try:
+	config = ConfigParser()
+	config.read('config.ini')
+
+	nasa_api_key = config.get('nasa', 'api_key')
+	nasa_api_url = config.get('nasa', 'api_url')
+
+except:
+	logger.exception('')
+print('DONE')
 
 # Getting todays date
 dt = datetime.now()
